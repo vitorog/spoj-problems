153c250666084b862a44e56587ece1c034c11665
Vitor Gomes <vitor.shade@gmail.com>
2013-04-09 20:35:32 -0300

    -Made some optimizations and finally got it to be accepted. There are still some optimizations that could be done, maybe i will try them later to see how much faster it runs. For example, the largest factorial could be saved so the search for the largest calculated factorial below N would not have to be done, we could just get this largets factorial and start calculating the factorial of N from it.

--- small_factorial/main.cpp
+++ small_factorial/main.cpp
@@ -13,7 +13,6 @@ public:
             int d=0;
             do{d=n%10;n_.push_front(d);n=n/10;}while(n);
         }else{n_.push_front(n);}}
-    B operator+(B&);
     B& operator=(const B&);
     B operator*(int);
     friend ostream& operator<<(ostream&,B&);
@@ -24,44 +23,23 @@ public:
 map<int,B> fh;
 ostream& operator<<(ostream& s,B& n){n.P();return s;}
 void B::P(){for(l::iterator it=n_.begin();it!=n_.end();it++){cout<<(int)(*it);}}
-B B::operator+(B& b){
-    l *a_op, *b_op;
-    if(n_.size()>=b.n_.size()){a_op=&n_;b_op=&b.n_;
-    }else{a_op=&b.n_;b_op=&n_;}
-    l::reverse_iterator a_it=a_op->rbegin(),b_it;
-    l rn;
-    int cc=0,nc=0,r;
-    for(b_it=b_op->rbegin();b_it!=b_op->rend();b_it++){
-        r=(*a_it)+(*b_it)+cc;
-        if(r>=10){
-            nc=r/10;
-            r=r%10; }
-        rn.push_front(r);
-        cc=nc;
-        nc=0;
-        a_it++;}
-    if(a_it !=a_op->rend()){
-        for(;a_it !=a_op->rend(); a_it++){
-            if(cc!=0){
-                r=(*a_it)+cc;
-                if(r>=10){
-                    nc=r/10;
-                    r=r%10;}
-                rn.push_front(r);
-                cc=nc;
-                nc=0;
-            }else{rn.push_front((*a_it));}
-        }
-        if(cc!=0){rn.push_front(cc);}
-    }else if(cc!=0){rn.push_front(cc);}
-    return B(rn);
-}
 B& B::operator=(const B& b){ n_=b.n_;i_=b.i_;return *this;}
 B B::operator*(int n){
-    B r(0),o(n_);
-    int c=0;
-    while(c<n){r=r+o;c++;}
-    return r;
+    int r,cc=0,nc=0;
+    l re;
+    for(l::reverse_iterator it=n_.rbegin();it!=n_.rend();it++){
+        r=((*it)*n)+cc;
+        if(r>=10){
+            nc=r/10;
+            r%=10;
+        }
+        re.push_front(r);cc=nc;nc=0;
+    }
+    while(cc!=0){
+        re.push_front(cc%10);
+        cc/=10;
+    }
+    return B(re);
 }
 void F(int n){
     if(fh.find(n)!=fh.end()){fh[n].P();}
@@ -69,14 +47,25 @@ void F(int n){
         B f(1);
         int i,j=1;
         if(n>2) for(j=n-1;j>=2;j--){if(fh.find(j)!=fh.end()){f=fh[j];break;}}
-        for(i=j+1;i<=n;i++){f=f*i;}
-        f.i_=n;fh[n]=f;
+        for(i=j+1;i<=n;i++){
+            f=f*i;
+            if(fh.find(i)!=fh.end()){
+                f.i_=i;fh[i]=f;
+            }
+        }
         cout<<f<<endl;}}
 void S(int n){
     long double f=1;
-    for(int i=2;i<=n;i++){f*=i;}
+    for(int i=2;i<=n;i++){
+        f*=i;
+        if(fh.find(i)!=fh.end()){
+            B fb(f);
+            fb.i_=i;
+            fh[i]=fb;
+        }
+    }
     std::cout<<fixed<<setprecision(0)<<f<<std::endl;
-    B fb=B(f);fb.i_=n;fh[n]=fb;}
+}
 int main(){
     int t,n,i;
     cin>>t;
